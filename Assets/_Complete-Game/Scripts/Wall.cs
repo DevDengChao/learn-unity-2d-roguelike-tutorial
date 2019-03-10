61a744e54dad826bde0f546d4fe5fe9ff1420e48
XieEDeHeiShou <2325690622@qq.com>
2019-03-10 11:51:07 +0800

    Reduce wall health point to 2

--- Assets/_Complete-Game/Scripts/Wall.cs
+++ Assets/_Complete-Game/Scripts/Wall.cs
@@ -2,12 +2,11 @@
 
 namespace Scripts {
     public class Wall : MonoBehaviour {
+        private int _hp = 2; //hit points for the wall.
         private SpriteRenderer _spriteRenderer; //Store a component reference to the attached SpriteRenderer.
         public AudioClip chopSound1; //1 of 2 audio clips that play when the wall is attacked by the player.
         public AudioClip chopSound2; //2 of 2 audio clips that play when the wall is attacked by the player.
         public Sprite dmgSprite; //Alternate sprite to display after Wall has been attacked by player.
-        public int hp = 3; //hit points for the wall.
-
 
         private void Awake() {
             //Get a component reference to the SpriteRenderer.
@@ -24,12 +23,14 @@ namespace Scripts {
             _spriteRenderer.sprite = dmgSprite;
 
             //Subtract loss from hit point total.
-            hp -= loss;
+            _hp -= loss;
+            print($"{name} at {transform.position} hp = {_hp}");
 
             //If hit points are less than or equal to zero:
-            if (hp <= 0)
-                //Disable the gameObject.
-                gameObject.SetActive(false);
+            if (_hp > 0) return;
+            print($"{name} at {transform.position} de-active");
+            //Disable the gameObject.
+            gameObject.SetActive(false);
         }
     }
 }
