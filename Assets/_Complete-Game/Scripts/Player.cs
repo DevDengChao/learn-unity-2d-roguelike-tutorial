e93255d73df41c53d53628cadc380bbeefca5451
XieEDeHeiShou <2325690622@qq.com>
2019-03-10 12:52:52 +0800

    Remove static modifier

--- Assets/_Complete-Game/Scripts/Player.cs
+++ Assets/_Complete-Game/Scripts/Player.cs
@@ -200,7 +200,7 @@ namespace Scripts {
 
 
         //Restart reloads the scene when called.
-        private static void Restart() {
+        private void Restart() {
             //Load the last scene loaded, in this case Main, the only scene in the game. And we load it in "Single" mode so it replace the existing one
             //and not load all the scene object in the current scene.
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex, LoadSceneMode.Single);
